556b3aa60c95ab9e742451590a187f8d4d18ac61
Naveenravi07 <gamerkidnav@gmail.com>
2023-07-31 01:10:31 +0530

    deeez

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 use std::env;
+use std::fs;
+use::std::process;
+use std::error::Error;
+
 
 fn main() {
-    let (query,filename) = read_input();
-    println!("filename = {} and query is : {}",filename,query);
+    let args : Vec<String> = env::args().collect();
+
+    let input_data = Config::new(&args).unwrap_or_else(|err|{
+        println!("problem in parsing arguments, {}",err);
+        process::exit(1);
+    });
+
+    println!("filename = {} and query is : {}",input_data.filename,input_data.query);
+   
+    if let Err(e) = run(input_data){
+        println!("Application Error, {}",e);
+        process::exit(1);
+    }
+
 }
 
-fn read_input()->(String,String){
-    let args : Vec<String> = env::args().collect();
-    if args.len()<3 {
-        panic!("You are such a dumbasss");
+struct Config{
+    filename:String,
+    query:String
+}
+
+impl  Config {
+    fn new(args:&[String])-> Result<Config,&str> {
+        if args.len() < 3 {
+            return Err("You are such a dumbass");
+        }
+        let filename = args[1].clone();
+        let query = args[2].clone(); 
+        Ok(Config {filename, query}) 
     }
-     (args[1].clone(),args[2].clone())
+}
+fn run (config:Config)->Result<(),Box<dyn Error>>{
+    let content = fs::read_to_string(config.filename)?;
+    println!("{}",content);
+    Ok(())
 }
