5a02f40b13ef99f79c20f315f59190e5e0a3cafc
Naveenravi07 <gamerkidnav@gmail.com>
2023-08-01 20:12:21 +0530

    Case sensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#[warn(dead_code)]
 use std::fs;
 use std::error::Error;
+use::std::env;
+
 
 pub struct Config{
     pub  filename:String,
-    pub query:String
+    pub query:String,
+    pub is_case_sensitive:bool
 }
 
 impl  Config {
@@ -13,19 +17,70 @@ impl  Config {
         }
         let filename = args[1].clone();
         let query = args[2].clone(); 
-        Ok(Config {filename, query}) 
+        let is_case_sensitive = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {filename, query, is_case_sensitive}) 
     }
 }
+
 pub fn run (config:Config)->Result<(),Box<dyn Error>>{
     let content = fs::read_to_string(config.filename)?;
-    println!("{}",content);
+    let search_result = if config.is_case_sensitive{
+        search(&content, &config.query)
+    }else{
+        search_case_insensitive(&content, &config.query)
+    };
+    for line in  search_result{
+        println!("{}",line);
+    }
     Ok(())
 }
-#[cfg(test)]
 
+pub fn search<'a>(content:&'a str,query:&str)->Vec<&'a str>{
+    let mut results = Vec::new();
+    for line in content.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(content:&'a str,query:&str)->Vec<&'a str>{
+    let mut results = Vec::new();
+    for line in content.lines(){
+        if line.to_lowercase().contains(&query.to_lowercase()){
+            results.push(line)
+        }
+    }
+    results
+}
+
+#[cfg(test)]
 mod tests{
     use super::*;
     
+    #[test]
+    fn case_sensitive_search(){
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        let query = "duct";
+        assert_eq!(vec!["safe, fast, productive."],search(contents,query));
+    }
+
+
+    #[test]
+    fn case_insensitive_search(){
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        let query = "duct";
+        assert_eq!(vec!["safe, fast, productive."],search(contents,query));
+    }
 
 }
 
